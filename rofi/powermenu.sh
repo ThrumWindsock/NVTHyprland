b4cfd68d84e52932eacd3f0b4a8d9c04495e6a3c
Thrum <thrum@tinsec.net>
2025-06-17 16:17:24 -0400

    new file:   hypr/hyprpaper/nvtbackground1.png 	new file:   hypr/hyprpaper/nvtbackground2.png 	new file:   hypr/hyprpaper/nvtbackground3.png 	new file:   hypr/hyprpaper/nvtbackground4.png 	new file:   hypr/hyprpaper/nvtbackground5.png 	new file:   hypr/wallpaperflip.sh 	new file:   rofi/power.rasi 	new file:   rofi/powermenu.sh

---
+++ rofi/powermenu.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+#
+# A rofi powered menu to execute power related action.
+# Uses: amixer mpc poweroff reboot rofi rofi-prompt
+
+power_off=''
+reboot=''
+lock=''
+suspend=''
+log_out=''
+
+chosen=$(printf '%s;%s;%s;%s;%s\n' "$power_off" "$reboot" "$lock" "$suspend" \
+                                   "$log_out" \
+    | rofi -theme-str '@import "power.rasi"' \
+           -dmenu \
+           -sep ';' \
+           -selected-row 2)
+
+case "$chosen" in
+    "$power_off")
+        shutdown now
+        ;;
+
+    "$reboot")
+        reboot
+        ;;
+
+    "$lock")
+        # TODO Add your lockscreen command.
+        ;;
+
+    "$suspend")
+        # Pause music and mute volume before suspending.
+        mpc --quiet pause
+        amixer set Master mute
+        # TODO Add your suspend command.
+        ;;
+
+    "$log_out")
+        pkill -u $USER
+        ;;
+
+    *) exit 1 ;;
+esac
